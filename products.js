e8615b2f0711df619458f290d6bad4357fb8536e
river10911 <river10911@gmail.com>
2026-06-09 16:13:03 +0800

    Create products.js

---
+++ products.js
@@ -0,0 +1,203 @@
+const products = {
+
+"iphone 17": {
+
+name:"iPhone 17",
+
+prices:[
+
+{
+country:"日本",
+flag:"🇯🇵",
+platform:"Amazon JP",
+twd:27800,
+link:"https://www.amazon.co.jp/s?k=iPhone+17"
+},
+
+{
+country:"韓國",
+flag:"🇰🇷",
+platform:"Coupang",
+twd:28400,
+link:"https://www.coupang.com/np/search?q=iPhone+17"
+},
+
+{
+country:"香港",
+flag:"🇭🇰",
+platform:"Apple HK",
+twd:28600,
+link:"https://www.apple.com/hk/"
+},
+
+{
+country:"台灣",
+flag:"🇹🇼",
+platform:"momo",
+twd:28900,
+link:"https://www.momoshop.com.tw/search/searchShop.jsp?keyword=iPhone+17"
+},
+
+{
+country:"美國",
+flag:"🇺🇸",
+platform:"Apple US",
+twd:29500,
+link:"https://www.apple.com/iphone/"
+}
+
+]
+
+},
+
+"iphone 17 pro": {
+
+name:"iPhone 17 Pro",
+
+prices:[
+
+{
+country:"日本",
+flag:"🇯🇵",
+platform:"Amazon JP",
+twd:35800,
+link:"https://www.amazon.co.jp/s?k=iPhone+17+Pro"
+},
+
+{
+country:"香港",
+flag:"🇭🇰",
+platform:"Apple HK",
+twd:36100,
+link:"https://www.apple.com/hk/"
+},
+
+{
+country:"韓國",
+flag:"🇰🇷",
+platform:"Coupang",
+twd:36400,
+link:"https://www.coupang.com/np/search?q=iPhone+17+Pro"
+},
+
+{
+country:"美國",
+flag:"🇺🇸",
+platform:"Apple US",
+twd:36900,
+link:"https://www.apple.com/iphone/"
+},
+
+{
+country:"台灣",
+flag:"🇹🇼",
+platform:"momo",
+twd:40900,
+link:"https://www.momoshop.com.tw/search/searchShop.jsp?keyword=iPhone+17+Pro"
+}
+
+]
+
+},
+
+"airpods pro": {
+
+name:"AirPods Pro",
+
+prices:[
+
+{
+country:"日本",
+flag:"🇯🇵",
+platform:"Amazon JP",
+twd:6200,
+link:"https://www.amazon.co.jp/s?k=AirPods+Pro"
+},
+
+{
+country:"韓國",
+flag:"🇰🇷",
+platform:"Coupang",
+twd:6500,
+link:"https://www.coupang.com/np/search?q=AirPods+Pro"
+},
+
+{
+country:"香港",
+flag:"🇭🇰",
+platform:"Apple HK",
+twd:6800,
+link:"https://www.apple.com/hk/"
+},
+
+{
+country:"美國",
+flag:"🇺🇸",
+platform:"Apple US",
+twd:7200,
+link:"https://www.apple.com/airpods-pro/"
+},
+
+{
+country:"台灣",
+flag:"🇹🇼",
+platform:"momo",
+twd:7490,
+link:"https://www.momoshop.com.tw/search/searchShop.jsp?keyword=AirPods+Pro"
+}
+
+]
+
+},
+
+"macbook air": {
+
+name:"MacBook Air",
+
+prices:[
+
+{
+country:"日本",
+flag:"🇯🇵",
+platform:"Amazon JP",
+twd:32900,
+link:"https://www.amazon.co.jp/s?k=MacBook+Air"
+},
+
+{
+country:"美國",
+flag:"🇺🇸",
+platform:"Apple US",
+twd:33900,
+link:"https://www.apple.com/macbook-air/"
+},
+
+{
+country:"香港",
+flag:"🇭🇰",
+platform:"Apple HK",
+twd:34200,
+link:"https://www.apple.com/hk/"
+},
+
+{
+country:"韓國",
+flag:"🇰🇷",
+platform:"Coupang",
+twd:34500,
+link:"https://www.coupang.com/np/search?q=MacBook+Air"
+},
+
+{
+country:"台灣",
+flag:"🇹🇼",
+platform:"momo",
+twd:34900,
+link:"https://www.momoshop.com.tw/search/searchShop.jsp?keyword=MacBook+Air"
+}
+
+]
+
+}
+
+};
