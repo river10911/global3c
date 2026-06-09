12a1ac22e09d72219b49bf6a6baf56abd94ae3d8
river10911 <river10911@gmail.com>
2026-06-09 16:29:04 +0800

    Update products.js

--- products.js
+++ products.js
@@ -2,226 +2,229 @@ const products = {
 
 "iphone 17": {
 
+```
 name:"iPhone 17",
 
 prices:[
 
-{
-country:"日本",
-flag:"🇯🇵",
-platform:"Amazon JP",
-twd:27800,
-link:"https://www.amazon.co.jp/s?k=iPhone+17"
-},
+    {
+        country:"日本",
+        flag:"🇯🇵",
+        platform:"Amazon JP",
+        localPrice:"¥124,800",
+        twd:27800,
+        link:"https://www.amazon.co.jp/s?k=iPhone+17"
+    },
 
-{
-country:"韓國",
-flag:"🇰🇷",
-platform:"Coupang",
-twd:28400,
-link:"https://www.coupang.com/np/search?q=iPhone+17"
-},
+    {
+        country:"韓國",
+        flag:"🇰🇷",
+        platform:"Coupang",
+        localPrice:"₩1,250,000",
+        twd:28400,
+        link:"https://www.coupang.com/np/search?q=iPhone+17"
+    },
 
-{
-country:"香港",
-flag:"🇭🇰",
-platform:"Apple HK",
-twd:28600,
-link:"https://www.apple.com/hk/"
-},
+    {
+        country:"香港",
+        flag:"🇭🇰",
+        platform:"Apple HK",
+        localPrice:"HK$6,899",
+        twd:28600,
+        link:"https://www.apple.com/hk/"
+    },
 
-{
-country:"台灣",
-flag:"🇹🇼",
-platform:"momo",
-twd:28900,
-link:"https://www.momoshop.com.tw/search/searchShop.jsp?keyword=iPhone+17"
-},
+    {
+        country:"台灣",
+        flag:"🇹🇼",
+        platform:"momo",
+        localPrice:"NT$28,900",
+        twd:28900,
+        link:"https://www.momoshop.com.tw/search/searchShop.jsp?keyword=iPhone+17"
+    },
 
-{
-country:"美國",
-flag:"🇺🇸",
-platform:"Apple US",
-twd:29500,
-link:"https://www.apple.com/iphone/"
-}
+    {
+        country:"美國",
+        flag:"🇺🇸",
+        platform:"Apple US",
+        localPrice:"US$799",
+        twd:29500,
+        link:"https://www.apple.com/iphone/"
+    }
 
 ]
+```
 
 },
 
 "iphone 17 pro": {
 
+```
 name:"iPhone 17 Pro",
 
 prices:[
 
-{
-    country:"日本",
-    flag:"🇯🇵",
-    platform:"Amazon JP",
+    {
+        country:"日本",
+        flag:"🇯🇵",
+        platform:"Amazon JP",
+        localPrice:"¥169,800",
+        twd:35800,
+        link:"https://www.amazon.co.jp/s?k=iPhone+17+Pro"
+    },
 
-    currency:"JPY",
-    originalPrice:"¥169,800",
+    {
+        country:"香港",
+        flag:"🇭🇰",
+        platform:"Apple HK",
+        localPrice:"HK$8,999",
+        twd:36100,
+        link:"https://www.apple.com/hk/"
+    },
 
-    twd:35800,
+    {
+        country:"韓國",
+        flag:"🇰🇷",
+        platform:"Coupang",
+        localPrice:"₩1,550,000",
+        twd:36400,
+        link:"https://www.coupang.com/np/search?q=iPhone+17+Pro"
+    },
 
-    link:"https://www.amazon.co.jp/s?k=iPhone+17+Pro"
-},
+    {
+        country:"美國",
+        flag:"🇺🇸",
+        platform:"Apple US",
+        localPrice:"US$1,099",
+        twd:36900,
+        link:"https://www.apple.com/iphone/"
+    },
 
-{
-    country:"香港",
-    flag:"🇭🇰",
-    platform:"Apple HK",
-
-    currency:"HKD",
-    originalPrice:"HK$8,999",
-
-    twd:36100,
-
-    link:"https://www.apple.com/hk/"
-},
-
-{
-    country:"韓國",
-    flag:"🇰🇷",
-    platform:"Coupang",
-
-    currency:"KRW",
-    originalPrice:"₩1,550,000",
-
-    twd:36400,
-
-    link:"https://www.coupang.com/np/search?q=iPhone+17+Pro"
-},
-
-{
-    country:"美國",
-    flag:"🇺🇸",
-    platform:"Apple US",
-
-    currency:"USD",
-    originalPrice:"US$1,099",
-
-    twd:36900,
-
-    link:"https://www.apple.com/iphone/"
-},
-
-{
-    country:"台灣",
-    flag:"🇹🇼",
-    platform:"momo",
-
-    currency:"TWD",
-    originalPrice:"NT$40,900",
-
-    twd:40900,
-
-    link:"https://www.momoshop.com.tw/search/searchShop.jsp?keyword=iPhone+17+Pro"
-}
+    {
+        country:"台灣",
+        flag:"🇹🇼",
+        platform:"momo",
+        localPrice:"NT$40,900",
+        twd:40900,
+        link:"https://www.momoshop.com.tw/search/searchShop.jsp?keyword=iPhone+17+Pro"
+    }
 
 ]
+```
 
 },
 
 "airpods pro": {
 
+```
 name:"AirPods Pro",
 
 prices:[
 
-{
-country:"日本",
-flag:"🇯🇵",
-platform:"Amazon JP",
-twd:6200,
-link:"https://www.amazon.co.jp/s?k=AirPods+Pro"
-},
+    {
+        country:"日本",
+        flag:"🇯🇵",
+        platform:"Amazon JP",
+        localPrice:"¥39,800",
+        twd:6200,
+        link:"https://www.amazon.co.jp/s?k=AirPods+Pro"
+    },
 
-{
-country:"韓國",
-flag:"🇰🇷",
-platform:"Coupang",
-twd:6500,
-link:"https://www.coupang.com/np/search?q=AirPods+Pro"
-},
+    {
+        country:"韓國",
+        flag:"🇰🇷",
+        platform:"Coupang",
+        localPrice:"₩289,000",
+        twd:6500,
+        link:"https://www.coupang.com/np/search?q=AirPods+Pro"
+    },
 
-{
-country:"香港",
-flag:"🇭🇰",
-platform:"Apple HK",
-twd:6800,
-link:"https://www.apple.com/hk/"
-},
+    {
+        country:"香港",
+        flag:"🇭🇰",
+        platform:"Apple HK",
+        localPrice:"HK$1,849",
+        twd:6800,
+        link:"https://www.apple.com/hk/"
+    },
 
-{
-country:"美國",
-flag:"🇺🇸",
-platform:"Apple US",
-twd:7200,
-link:"https://www.apple.com/airpods-pro/"
-},
+    {
+        country:"美國",
+        flag:"🇺🇸",
+        platform:"Apple US",
+        localPrice:"US$249",
+        twd:7200,
+        link:"https://www.apple.com/airpods-pro/"
+    },
 
-{
-country:"台灣",
-flag:"🇹🇼",
-platform:"momo",
-twd:7490,
-link:"https://www.momoshop.com.tw/search/searchShop.jsp?keyword=AirPods+Pro"
-}
+    {
+        country:"台灣",
+        flag:"🇹🇼",
+        platform:"momo",
+        localPrice:"NT$7,490",
+        twd:7490,
+        link:"https://www.momoshop.com.tw/search/searchShop.jsp?keyword=AirPods+Pro"
+    }
 
 ]
+```
 
 },
 
 "macbook air": {
 
+```
 name:"MacBook Air",
 
 prices:[
 
-{
-country:"日本",
-flag:"🇯🇵",
-platform:"Amazon JP",
-twd:32900,
-link:"https://www.amazon.co.jp/s?k=MacBook+Air"
-},
+    {
+        country:"日本",
+        flag:"🇯🇵",
+        platform:"Amazon JP",
+        localPrice:"¥164,800",
+        twd:32900,
+        link:"https://www.amazon.co.jp/s?k=MacBook+Air"
+    },
 
-{
-country:"美國",
-flag:"🇺🇸",
-platform:"Apple US",
-twd:33900,
-link:"https://www.apple.com/macbook-air/"
-},
+    {
+        country:"美國",
+        flag:"🇺🇸",
+        platform:"Apple US",
+        localPrice:"US$999",
+        twd:33900,
+        link:"https://www.apple.com/macbook-air/"
+    },
 
-{
-country:"香港",
-flag:"🇭🇰",
-platform:"Apple HK",
-twd:34200,
-link:"https://www.apple.com/hk/"
-},
+    {
+        country:"香港",
+        flag:"🇭🇰",
+        platform:"Apple HK",
+        localPrice:"HK$8,999",
+        twd:34200,
+        link:"https://www.apple.com/hk/"
+    },
 
-{
-country:"韓國",
-flag:"🇰🇷",
-platform:"Coupang",
-twd:34500,
-link:"https://www.coupang.com/np/search?q=MacBook+Air"
-},
+    {
+        country:"韓國",
+        flag:"🇰🇷",
+        platform:"Coupang",
+        localPrice:"₩1,590,000",
+        twd:34500,
+        link:"https://www.coupang.com/np/search?q=MacBook+Air"
+    },
 
-{
-country:"台灣",
-flag:"🇹🇼",
-platform:"momo",
-twd:34900,
-link:"https://www.momoshop.com.tw/search/searchShop.jsp?keyword=MacBook+Air"
-}
+    {
+        country:"台灣",
+        flag:"🇹🇼",
+        platform:"momo",
+        localPrice:"NT$34,900",
+        twd:34900,
+        link:"https://www.momoshop.com.tw/search/searchShop.jsp?keyword=MacBook+Air"
+    }
 
 ]
+```
 
 }
 
