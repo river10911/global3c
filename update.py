e583cd3fbffc1aec158660007b8a3f463883441a
river10911 <river10911@gmail.com>
2026-06-10 09:17:39 +0800

    Update update.py

--- update.py
+++ update.py
@@ -1,1 +1,35 @@
+import json
+import requests
+from datetime import datetime
+
+URL = "https://open.er-api.com/v6/latest/USD"
+
+response = requests.get(URL, timeout=30)
+data = response.json()
+
+exchange = {
+"updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+"USD": 1,
+"TWD": data["rates"]["TWD"],
+"JPY": data["rates"]["JPY"],
+"HKD": data["rates"]["HKD"],
+"KRW": data["rates"]["KRW"]
+}
+
+with open(
+"data/exchange.json",
+"w",
+encoding="utf-8"
+) as f:
+
+```
+json.dump(
+    exchange,
+    f,
+    ensure_ascii=False,
+    indent=2
+)
+```
+
+print("exchange.json updated successfully")
 
