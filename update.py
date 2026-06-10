d94f3628f5eb770945e3313aa83c7072e255dac7
river10911 <river10911@gmail.com>
2026-06-10 09:55:22 +0800

    Update update.py

--- update.py
+++ update.py
@@ -8,20 +8,20 @@ response = requests.get(url, timeout=30)
 data = response.json()
 
 exchange = {
-"updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-"USD": 1,
-"TWD": data["rates"]["TWD"],
-"JPY": data["rates"]["JPY"],
-"HKD": data["rates"]["HKD"],
-"KRW": data["rates"]["KRW"]
+    "updated_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    "USD": 1,
+    "TWD": data["rates"]["TWD"],
+    "JPY": data["rates"]["JPY"],
+    "HKD": data["rates"]["HKD"],
+    "KRW": data["rates"]["KRW"]
 }
 
 with open("data/exchange.json", "w", encoding="utf-8") as f:
-json.dump(
-exchange,
-f,
-ensure_ascii=False,
-indent=2
-)
+    json.dump(
+        exchange,
+        f,
+        ensure_ascii=False,
+        indent=2
+    )
 
 print("exchange.json updated")
